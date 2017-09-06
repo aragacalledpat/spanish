b1ea89e3d184c66b8b70fcfe0087ed3c3e83451a
Andrew Pappas <aragacalledpappas@gmail.com>
2017-09-05 20:49:40 -0400

    copy things over

--- spanish-app/src/app/app.component.ts
+++ spanish-app/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
+  words:Word[];
+
+  constructor(private http: Http)
+  {
+    this.loadVocab().subscribe(data => this.words = data, error =>console.log(error))
+  }
+
+  loadVocab() :Observable<Word[]>
+  {
+    return this.http
+    .get(`./assets/words.json`)
+    .map(response => response.json().words)
+  }
+}
+
+class Word {
+  id:string;
+  spanish:string;
+  english:string;
 }
